e6bc59d4f5e2ccef5b30b7746a3f87f1b0f71078
tonsellmann <91646543+tonsellmann@users.noreply.github.com>
2025-09-26 06:28:22 -0300

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-echo "--- INÍCIO DO BUILD SCRIPT ---"
-pip install -r requirements.txt
-echo "--- A EXECUTAR O TREINO ---"
-python treinar_modelos.py
-echo "--- BUILD SCRIPT CONCLUÍDO ---"
